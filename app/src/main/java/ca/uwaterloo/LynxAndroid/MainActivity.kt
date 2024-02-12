40c8169cda1c70b60083b866c9f156cbe2278b7d
Adrian Jendo <ajendo@uwaterloo.ca>
2024-02-12 01:24:23 -0500

    Formatting and styling

--- app/src/main/java/ca/uwaterloo/LynxAndroid/MainActivity.kt
+++ app/src/main/java/ca/uwaterloo/LynxAndroid/MainActivity.kt
@@ -3,13 +3,21 @@ package ca.uwaterloo.LynxAndroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ca.uwaterloo.LynxAndroid.ui.theme.LynxAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    UnlockButton()
                 }
             }
         }
@@ -30,17 +38,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun UnlockButton() {
+    MaterialTheme {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Button(
+                onClick = { /* Handle button click */ },
+                modifier = Modifier
+                    .padding(16.dp)
+                    .height(50.dp)
+                    .width(200.dp)
+            ) {
+                Text(
+                    text = "Unlock",
+                    fontSize = 18.sp
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LynxAndroidTheme {
-        Greeting("Android")
-    }
+fun UnlockButtonPreview() {
+    UnlockButton()
 }
